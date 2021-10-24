664cfe388c5721c1fea7a19b6afd2c78a4687bb2
Ravi Soni <77038756+ravi020997@users.noreply.github.com>
2021-10-24 19:38:29 +0530

    Day1 Q2 Solution
    Q2

---
+++ coding-challenges/week02/Q2.py
@@ -0,0 +1,22 @@
+#Writing Calculator with addition operator
+
+a = 10 
+b= 5 
+print(a+b)
+
+#Writing Calculator with subtraction operator
+c= 15 
+d = 5 
+print (c-d)
+
+#Writing Calculator with Multiplication operator
+
+m = 6 
+n = 5 
+print(m*n)
+
+#Writing Calculator with Division operator
+
+j = 10
+k = 2
+print(j/k)
